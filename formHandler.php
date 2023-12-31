40b90874d4563e5c52c32441643cb932fa957968
Tasneem <tasneemayham956@gmail.com>
2023-12-31 23:55:23 +0300

    some change

--- formHandler.php
+++ formHandler.php
@@ -17,5 +17,5 @@ $headers =  "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
-header("Location: contact.html");
+header("Location: fillings.html");
 ?>
