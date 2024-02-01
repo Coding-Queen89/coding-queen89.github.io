bff1a9e8994a45996b1540e41b65be5ae3d8a559
Tasneem <tasneemayham956@gmail.com>
2024-02-01 19:08:42 +0300

    Added Hangman and Menu
    For now the languages on HTML and js has been commented because I am not convinced by their reasoning.

--- main.js
+++ main.js
@@ -90,3 +90,15 @@ hiddenElements3.forEach((el)=> observer3.observe(el));
 //       $('video').attr('autoplay');
 //     }
 //   });
+
+//----------------------------LANGUAGESSSS------------
+
+
+// const dropdow = document.querySelector(".drop-down");
+// const list = document.querySelector(".listLang");
+// const selected = document.querySelector(".selected");
+// const selectedImg = document.querySelector(".selectedImg");
+
+// dropdow.addEventListener('click', () => {
+//     list.classList.toggle('showLangList');
+// })
