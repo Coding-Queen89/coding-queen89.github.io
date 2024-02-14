7f81782508218134a78dfc4d16983840f8831a22
Tasneem <tasneemayham956@gmail.com>
2024-02-14 19:27:00 +0300

    history

--- elite-patients.js
+++ elite-patients.js
@@ -34,3 +34,10 @@ var spans = wrapper.getElementsByTagName("span");
 for(var i=0; i<spans.length; i++){
     spans[i].style.animationDelay = i*80+"ms";
 }
+var wrapper = document.getElementsByClassName("text-animation2")[0];
+wrapper.innerHTML = wrapper.textContent.replace(/./g,"<span>$&</span>");
+
+var spans = wrapper.getElementsByTagName("span");
+for(var i=0; i<spans.length; i++){
+    spans[i].style.animationDelay = i*90+"ms";
+}
