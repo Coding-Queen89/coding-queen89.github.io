1633a9bb723f27900b3731f14835d14e0a09a338
Tasneem <tasneemayham956@gmail.com>
2024-02-19 17:07:15 +0300

    Adding Content Pages

--- programs.js
+++ programs.js
@@ -1,6 +1,7 @@
 var lastScrollTop = 0;
 window.onload = function () {
     var navbar = document.querySelector("header");
+    var offer = document.querySelector(".offer");
     window.addEventListener('scroll', function (e) {
         if (window.pageYOffset > 10) {
             navbar.classList.add('is-scrolling');
@@ -10,20 +11,22 @@ window.onload = function () {
         var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
         if (scrollTop > lastScrollTop) {
             navbar.style.top = "-140px";
+            offer.style.top = "40px";
         } else {
             navbar.style.top = "0";
+            offer.style.top = "90px"
         }
         lastScrollTop = scrollTop;
     });
 
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.navlist');
-    const body = document.querySelector('body');
+    const main = document.querySelector('.main');
 
     menu_btn.addEventListener('click', function () {
+        main.classList.toggle('blur');
         menu_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
-        body.classList.toggle('uncover');
     });
 }
 gsap.from('.logo div', {
